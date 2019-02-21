fa182daa68ebc9dbbd05101d8201a3a1bb11b6fc
cse-loaner <cse-loaner@macbook-air.lan>
2019-02-20 20:52:23 -0800

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by cse-loaner on 2/20/19.
+//  Copyright © 2019 uw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
